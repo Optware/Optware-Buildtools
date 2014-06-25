1b45d5b3dc77837eb2b29961f06cfae5fb7909d5
Sebastian <SebastianUlmer@gmx.de>
2014-06-25 14:41:42 +0200

    Initial checkin

---
+++ install_prerequirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install gcc autoconf automake bison flex gettext texi2html texinfo pkg-config g++ libtool tcl8.5 unzip make cvs pkg-config rsync libglib2.0-dev automake1.9 sudo patch bzip2 gzip wget sed subversion ncurses-base ncurses-bin zlib-bin original-awk libncurses5 libncurses5-dev zlib1g zlib1g-dev zlibc gawk
+
+
