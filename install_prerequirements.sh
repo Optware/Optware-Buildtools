e9e4925c09d8cd23bf9650b9c8777060e51f3643
Sebastian <SebastianUlmer@gmx.de>
2014-10-05 10:46:00 +0200

    Minor corrections

--- install_prerequirements.sh
+++ install_prerequirements.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+#--------------------------------------
+# Script to install all packages needed for optware compiling for Ubuntu
+#
+# by Sebastian ULmer
+#--------------------------------------
+#  Usage: 
+#   ./install_prerequirements
+#--------------------------------------
 sudo apt-get install gcc autoconf automake bison flex gettext texi2html texinfo pkg-config g++ libtool tcl8.5 unzip make cvs pkg-config rsync libglib2.0-dev automake1.9 sudo patch bzip2 gzip wget sed subversion ncurses-base ncurses-bin zlib-bin original-awk libncurses5 libncurses5-dev zlib1g zlib1g-dev zlibc gawk
-
 
