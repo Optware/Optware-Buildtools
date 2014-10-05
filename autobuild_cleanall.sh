69c6ee1cf09f8a11e03e8c49f051cc9d5c95ffc4
Sebastian <SebastianUlmer@gmx.de>
2014-10-05 10:45:39 +0200

    update to use git and GitHub

--- autobuild_cleanall.sh
+++ autobuild_cleanall.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-
+#--------------------------------------
+# Script to clean one package (or all packages) for optware
+# Initially updates the svn from trunk
+# 
+# by Sebastian Ulmer
+#
+#--------------------------------------
+#  Initially: 
+#    Change the 'optware_folder' to point to a directory name you want to have. 
+#    Directory will be created. If changed from default, it needs to be changed in all scripts
+#  Usage: 
+#   ./autoclean_all.sh    -> attempts to remove all file for all plattforms
+#--------------------------------------
 export OPTWARE_TARGET="nslu2"
+
+# Folder for optware
+optware_folder="optware"
 
 #TARGETS="ddwrt ds101 nslu2 ds101 ds101g syno-x07 openwrt-brcm24 sheevaplug wdtv slugos6be syno1142ppc824x "
 #ls platforms/ | grep packages | sed -e 's/packages-//' | sed -e 's/.mk//'
@@ -8,22 +23,22 @@ export OPTWARE_TARGET="nslu2"
 #first get reference platform for platform list
 i="default"
 export OPTWARE_TARGET=$i
-[ -d ~/optware/$i ] || mkdir -p ~/optware/$i
-[ ! -h ~/optware/$i/downloads ] && rm -fR ~/optware/$i/downloads && ln -s ~/optware/downloads ~/optware/$i/downloads
-cd ~/optware/$i
-svn co https://svn.nslu2-linux.org/svnroot/optware/trunk ~/optware/$i
+[ -d ~/$optware_folder/$i ] || mkdir -p ~/$optware_folder/$i
+[ ! -h ~/$optware_folder/$i/downloads ] && rm -fR ~/$optware_folder/$i/downloads && ln -s ~/$optware_folder/downloads ~/$optware_folder/$i/downloads
+cd ~/$optware_folder/$i
 
-TARGETS=`ls ~/optware/default/platforms/ | grep packages | sed -e 's/packages-//' | sed -e 's/.mk//'`
+git clone https://github.com/Optware/Optware ~/$optware_folder/$i
+
+TARGETS=`ls ~/$optware_folder/default/platforms/ | grep packages | sed -e 's/packages-//' | sed -e 's/.mk//'`
 
 
 for i in $TARGETS
 do
 {
-   #echo "cleaning ... ~/optware/$i "
-   [ -d ~/optware/builddir/$i ]&& echo "cleaning ... ~/optware/$i " && rm -rf ~/optware/builddir/$i
-   [ -e ~/optware/make_$i.log ] && rm ~/optware/make_$i.log
+   echo "cleaning ... ~/$optware_folder/$i "
+   [ -d ~/$optware_folder/builddir/$i ]&& echo "cleaning ... ~/$optware_folder/$i " && rm -rf ~/$optware_folder/builddir/$i
+   [ -e ~/$optware_folder/make_$i.log ] && rm ~/$optware_folder/make_$i.log
 }
 done
-
 
 
